d8a7610f61bd1c58695c51a4275a238d3b34b61e
Aldren Terante <me@thecodingwhale.com>
2014-09-05 19:15:59 +0800

    update profile display data

--- app/views/packages/rydurham/sentinel/users/create.blade.php
+++ app/views/packages/rydurham/sentinel/users/create.blade.php
@@ -9,55 +9,59 @@ Register
 {{-- Content --}}
 @section('content')
 <div class="row">
-    <div class="col-md-4 col-md-offset-4">
+    <div class="col-md-12">
         {{ Form::open(array('action' => 'Sentinel\UserController@store')) }}
 
             <h2>Register New Account</h2>
+            <div class="row">
+                <div class="col-md-6">
+                    <h3>Personal Information</h3>
 
-            <h3>Personal Information</h3>
+                    <div class="form-group {{ ($errors->has('first_name')) ? 'has-error' : '' }}">
+                        {{ Form::text('first_name', null, array('class' => 'form-control', 'placeholder' => 'First Name')) }}
+                        {{ ($errors->has('first_name') ? $errors->first('first_name') : '') }}
+                    </div>
 
-            <div class="form-group {{ ($errors->has('first_name')) ? 'has-error' : '' }}">
-                {{ Form::text('first_name', null, array('class' => 'form-control', 'placeholder' => 'First Name')) }}
-                {{ ($errors->has('first_name') ? $errors->first('first_name') : '') }}
+                    <div class="form-group {{ ($errors->has('last_name')) ? 'has-error' : '' }}">
+                        {{ Form::text('last_name', null, array('class' => 'form-control', 'placeholder' => 'Last Name')) }}
+                        {{ ($errors->has('last_name') ? $errors->first('last_name') : '') }}
+                    </div>
+
+                    <div class="form-group {{ ($errors->has('contact')) ? 'has-error' : '' }}">
+                        {{ Form::text('contact', null, array('class' => 'form-control', 'placeholder' => 'Contact')) }}
+                        {{ ($errors->has('contact') ? $errors->first('contact') : '') }}
+                    </div>
+
+                    <div class="form-group {{ ($errors->has('company')) ? 'has-error' : '' }}">
+                        {{ Form::text('company', null, array('class' => 'form-control', 'placeholder' => 'Company')) }}
+                        {{ ($errors->has('company') ? $errors->first('company') : '') }}
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <h3>Account Information</h3>
+
+                    <div class="form-group {{ ($errors->has('email')) ? 'has-error' : '' }}">
+                        {{ Form::text('email', null, array('class' => 'form-control', 'placeholder' => 'E-mail')) }}
+                        {{ ($errors->has('email') ? $errors->first('email') : '') }}
+                    </div>
+
+                    <div class="form-group {{ ($errors->has('username')) ? 'has-error' : '' }}">
+                        {{ Form::text('username', null, array('class' => 'form-control', 'placeholder' => 'Username')) }}
+                        {{ ($errors->has('username') ? $errors->first('username') : '') }}
+                    </div>
+
+                    <div class="form-group {{ ($errors->has('password')) ? 'has-error' : '' }}">
+                        {{ Form::password('password', array('class' => 'form-control', 'placeholder' => 'Password')) }}
+                        {{ ($errors->has('password') ?  $errors->first('password') : '') }}
+                    </div>
+
+                    <div class="form-group {{ ($errors->has('password_confirmation')) ? 'has-error' : '' }}">
+                        {{ Form::password('password_confirmation', array('class' => 'form-control', 'placeholder' => 'Confirm Password')) }}
+                        {{ ($errors->has('password_confirmation') ?  $errors->first('password_confirmation') : '') }}
+                    </div>
+                </div>
             </div>
-
-            <div class="form-group {{ ($errors->has('last_name')) ? 'has-error' : '' }}">
-                {{ Form::text('last_name', null, array('class' => 'form-control', 'placeholder' => 'Last Name')) }}
-                {{ ($errors->has('last_name') ? $errors->first('last_name') : '') }}
-            </div>
-
-            <div class="form-group {{ ($errors->has('contact')) ? 'has-error' : '' }}">
-                {{ Form::text('contact', null, array('class' => 'form-control', 'placeholder' => 'Contact')) }}
-                {{ ($errors->has('contact') ? $errors->first('contact') : '') }}
-            </div>
-
-            <div class="form-group {{ ($errors->has('company')) ? 'has-error' : '' }}">
-                {{ Form::text('company', null, array('class' => 'form-control', 'placeholder' => 'Company')) }}
-                {{ ($errors->has('company') ? $errors->first('company') : '') }}
-            </div>
-
-            <h3>Account Information</h3>
-
-            <div class="form-group {{ ($errors->has('email')) ? 'has-error' : '' }}">
-                {{ Form::text('email', null, array('class' => 'form-control', 'placeholder' => 'E-mail')) }}
-                {{ ($errors->has('email') ? $errors->first('email') : '') }}
-            </div>
-
-            <div class="form-group {{ ($errors->has('username')) ? 'has-error' : '' }}">
-                {{ Form::text('username', null, array('class' => 'form-control', 'placeholder' => 'Username')) }}
-                {{ ($errors->has('username') ? $errors->first('username') : '') }}
-            </div>
-
-            <div class="form-group {{ ($errors->has('password')) ? 'has-error' : '' }}">
-                {{ Form::password('password', array('class' => 'form-control', 'placeholder' => 'Password')) }}
-                {{ ($errors->has('password') ?  $errors->first('password') : '') }}
-            </div>
-
-            <div class="form-group {{ ($errors->has('password_confirmation')) ? 'has-error' : '' }}">
-                {{ Form::password('password_confirmation', array('class' => 'form-control', 'placeholder' => 'Confirm Password')) }}
-                {{ ($errors->has('password_confirmation') ?  $errors->first('password_confirmation') : '') }}
-            </div>
-            
+          
             {{ Form::submit('Register', array('class' => 'btn btn-primary')) }}
             
         {{ Form::close() }}
--- app/views/template/profile-createdlist.blade.php
+++ app/views/template/profile-createdlist.blade.php
@@ -1,6 +1,21 @@
-            			<div class="row page-category">
-            				<h2 class="page-title">Current Project<br /><small>support and share new projects or inventions</small></h2>
-            			</div>
+                              <div class="row page-category">
+                                    <div class="col-sm-6 col-md-9 no-padding">
+                                          <h2 class="page-title">Current Project<br /><small>support and share new projects or inventions</small></h2>
+                                    </div>
+                                    <div class="col-sm-6 col-md-3 no-padding">
+                                          <div id="dropdown-custom-trigger" class="dropdown dropdown-custom">
+                                                <button class="btn btn-default btn-block dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown">
+                                                      Sorted By
+                                                      <span class="caret"></span>
+                                                </button>
+                                                <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1" style="min-width: 285px;">
+                                                      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>
+                                                      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
+                                                      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>
+                                                </ul>
+                                          </div>
+                                    </div>                    
+                              </div>
             			<div class="row">
             				<div class="col-sm-12">
             					<div class="table-custom">
@@ -26,14 +41,14 @@
             									<tr>
             										<td><a href="/single-page.html">Glass Bread Toaster</a></td>
             										<td>July 6, 2014</td>
-            										<td><span class="font-ligth-blue">5 Days | 3 Hours | 40 Mins</span></td>
+            										<td><span class="font-ligth-blue">Completed</span></td>
             										<td>US$ 2500</td>
             										<td><a href="#">Edit</a> | <a href="#" class="font-red">Delete</a></td>
             									</tr>
             									<tr>
             										<td><a href="/single-page.html">Glass Bread Toaster</a></td>
             										<td>July 6, 2014</td>
-            										<td><span class="font-ligth-blue">5 Days | 3 Hours | 40 Mins</span></td>
+            										<td><span class="font-ligth-blue">Pending</span></td>
             										<td>US$ 2500</td>
             										<td><a href="#">Edit</a> | <a href="#" class="font-red">Delete</a></td>
             									</tr>
